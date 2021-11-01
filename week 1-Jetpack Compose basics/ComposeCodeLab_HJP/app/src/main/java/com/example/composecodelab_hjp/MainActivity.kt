2d99b3c71c49fdd0f58c49c96c233efcbb7dbe6c
jpoh281 <hjp4869@naver.com>
2021-11-01 22:35:43 +0900

    11. Animating your list

--- week 1-Jetpack Compose basics/ComposeCodeLab_HJP/app/src/main/java/com/example/composecodelab_hjp/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeCodeLab_HJP/app/src/main/java/com/example/composecodelab_hjp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.composecodelab_hjp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,7 +34,13 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
 
     var expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -41,7 +50,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
